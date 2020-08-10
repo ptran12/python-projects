8b05b6012909687b63635cf5d2a7fdd3e881a891
ptran12 <phitran.722@gmail.com>
2020-08-10 15:46:27 -0500

    testing matplot

--- python_data_analysis/data_analysis.py
+++ python_data_analysis/data_analysis.py
@@ -10,4 +10,16 @@ df_second_shift = pd.read_excel(excel_file_1, sheet_name = 'second')
 df_third_shift = pd.read_excel(excel_file_2)
 
 
-print(df_first_shit)
+# print(df_first_shit)
+# print(df_first_shit['Product'])
+
+df_all = pd.concat([df_first_shit, df_second_shift, df_third_shift])
+
+pivot = df_all.groupby(['Shift']).mean()
+shift_productivity = pivot.loc[:,'Production Run Time (Min)': 'Products Produced (Units)']
+# loc -> lets us select mutiple rows of data instead of just one 
+# : -> represents that we want every row 
+print(shift_productivity)
+
+shift_productivity.plot(kind='bar')
+plt.show()
