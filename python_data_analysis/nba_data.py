c9f49f582204667ae310571135af2e0d6fc1e970
ptran12 <phitran.722@gmail.com>
2020-08-10 16:39:54 -0500

    commit

--- python_data_analysis/nba_data.py
+++ python_data_analysis/nba_data.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 excel_file_1 = '2018-2019_NBA Player_Stats.xlsx'
 
-df_nba_data = pd.read_excel(excel_file_1)
+df_nba_data = pd.read_excel(excel_file_1, sheet_name = 'Sheet1')
 
-print(df_nba_data)
-print(df_nba_data['TEAM'])
+# print(df_nba_data)
+print(df_nba_data.iloc[1])
+# print(df_nba_data.head(2))
+
 
 # pivot = df_nba_data.groupby(['PPGPointsPoints per game.']).mean()
 # print(pivot)
