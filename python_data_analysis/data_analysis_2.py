fab41de8e93803ce886beff54ede526eccd3ab02
ptran12 <phitran.722@gmail.com>
2020-08-12 11:41:02 -0500

    testing pandas

--- python_data_analysis/data_analysis_2.py
+++ python_data_analysis/data_analysis_2.py
@@ -5,4 +5,11 @@ excel_file_path = 'pmsm_temperature_data.csv'
 df = pd.read_csv(excel_file_path)
 print(df.columns)
 
-# testing commit 
+df_info = df.info()
+print(df_info)
+
+print(df.describe()['i_d'])
+
+grouped_df = df.groupby(['profile_id']).max()
+print (grouped_df['torque'])
+# max torque grouped on the profile_ids
