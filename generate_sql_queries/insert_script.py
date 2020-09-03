9640b47cffaadfb488a6501b3e968c71e21e4344
ptran12 <phitran.722@gmail.com>
2020-09-03 09:13:30 -0500

    generating sql queries

--- generate_sql_queries/insert_script.py
+++ generate_sql_queries/insert_script.py
@@ -1,10 +1,14 @@
 import pandas as pd
-import csv
+import csv as cs
 
-Table_Name = 'Customers'
+Table_Name = 'NBA_Players'
 
 df = pd.read_csv('test.csv', header = 0)
+# print(df.head())
 
-print (df.head())
-
+# create output file 
+with open("output.txt", "w"):
+    for index, row in df.iterrows():
+        print('INSET INTO ' + Table_Name + '(\'Passenger\',\'Pclass\',\'Name\') VALUES (',
+            row['Passenger'], ',',row['Pclass'],',\''+row['Name']+'\');', file=open("output.text", "a"))
 
